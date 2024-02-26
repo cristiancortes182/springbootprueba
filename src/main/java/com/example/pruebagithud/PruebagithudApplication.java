3c8949457934961c10fe9332d0e625b81e61697c
cristiancortes182 <32282625+cristiancortes182@users.noreply.github.com>
2024-02-26 17:40:52 -0500

    inizializing spring boot

---
+++ src/main/java/com/example/pruebagithud/PruebagithudApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebagithud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebagithudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebagithudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebagithud/PruebagithudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebagithud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebagithudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
