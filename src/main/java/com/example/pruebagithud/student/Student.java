a30f5a70c557dc692aee791812ac92160dca1a19
cristiancortes182 <32282625+cristiancortes182@users.noreply.github.com>
2024-02-29 10:00:58 -0500

    nuevo mensaje 12

--- src/main/java/com/example/pruebagithud/student/Student.java
+++ src/main/java/com/example/pruebagithud/student/Student.java
@@ -5,8 +5,10 @@ import java.time.LocalDate;
 public class Student {
 
     private String firstname;
-
+    private String lastname;
     private LocalDate data;
 
+    private String email;
 
+    private int age;
 }
