2544a7e54ca125a54b081c007469a886497537b4
cristiancortes182 <32282625+cristiancortes182@users.noreply.github.com>
2024-02-29 10:10:39 -0500

    completo

--- src/main/java/com/example/pruebagithud/student/Student.java
+++ src/main/java/com/example/pruebagithud/student/Student.java
@@ -11,4 +11,55 @@ public class Student {
     private String email;
 
     private int age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(String firstname, String lastname, LocalDate data, String email, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.data = data;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {
+    }
 }
--- src/main/java/com/example/pruebagithud/student/StudentController.java
+++ src/main/java/com/example/pruebagithud/student/StudentController.java
@@ -3,10 +3,27 @@ package com.example.pruebagithud.student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class StudentController {
 
 
+    public List<Student> lista(){
+
+        return List.of(
+            new Student(
+                "cris",
+                    "cortes",
+                    LocalDate.now(),
+                    "@elloco",
+                    1
+
+
+            )
+        );
+    }
 
 }
