d9fe6ae779e5aa59b93277f233a0956e7a827890
cristiancortes182 <32282625+cristiancortes182@users.noreply.github.com>
2024-02-29 09:56:46 -0500

    nuevo mensaje 1

---
+++ src/main/java/com/example/pruebagithud/student/Student.java
@@ -0,0 +1,14 @@
+package com.example.pruebagithud.student;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private String firstname;
+    private String lastname;
+    private LocalDate data;
+
+    private String email;
+
+    private int age;
+}
